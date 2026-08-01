6f595d47642c6edea4a7f009f0a6cafa6585968b
Kate <159861304+kateslines@users.noreply.github.com>
2026-07-31 21:42:24 -0400

    Update script.user.js
    Removed www in grundos.cafe links

--- script.user.js
+++ script.user.js
@@ -4,8 +4,8 @@
 // @version      0.7
 // @description  Extend features of the stamp album on Grundo's Café
 // @author       supercow64, eleven, rowanberryyyy
-// @match        https://www.grundos.cafe/stamps/album/?page_id=*
-// @icon         https://www.grundos.cafe/static/images/favicon.66a6c5f11278.ico
+// @match        https://grundos.cafe/stamps/album/?page_id=*
+// @icon         https://grundos.cafe/static/images/favicon.66a6c5f11278.ico
 // @grant        none
 // @license      MIT
 // ==/UserScript==
